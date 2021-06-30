f5a61d8b369161ef5b55d4a5c4a5c5a424670532
lukiszki <81814218+lukiszki@users.noreply.github.com>
2021-06-30 11:53:53 +0200

    E

--- NeuralCar/Assets/Scripts/Network/SaveSystem.cs
+++ NeuralCar/Assets/Scripts/Network/SaveSystem.cs
@@ -10,12 +10,12 @@ public static class SaveSystem
     public static void SaveValues(Values val)
     {
         string[] export = { JsonUtility.ToJson(val) };
-        string path = "D:/NeuralCar/values/values.json";
+        string path = "E:/NeuralCar/values/values.json";
         File.WriteAllLines(path,export);
     }
     public static Values LoadValues()
     {
-        string path = "D:/NeuralCar/values/values.json";
+        string path = "E:/NeuralCar/values/values.json";
         string importData = File.ReadAllText(path);
         return JsonUtility.FromJson<Values>(importData);
     }
